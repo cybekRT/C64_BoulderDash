bf48e330fd8c635dbad268a2a4d175876e4732c5
cybek <cybek@wyobraz.net>
2022-04-10 02:19:40 +0200

    Slow the counters

--- main.asm
+++ main.asm
@@ -1,6 +1,10 @@
 ;.byte $00, $10
 .byte $01, $08
 
+CNT1 = $2a
+CNT2 = $2b
+#define CNT2_VAL #$b0
+
 * = $0801
 
 ;.asc "sys 4096"
@@ -9,18 +13,29 @@
 
 main:
 	lda	#$0
-	sta	$2a
+	sta	CNT1
+	lda	CNT2_VAL
+	sta	CNT2
 loop:
 	lda	$d020
 	clc
 	adc	#$1
 	sta	$d020
 
-	lda	$2a
+	lda	CNT1
 	clc
 	adc	#$1
-	sta	$2a
+	sta	CNT1
 	bcc	loop
+
+	lda	CNT2
+	clc
+	adc	#$1
+	sta	CNT2
+	bcc	loop
+
+	lda	CNT2_VAL
+	sta	CNT2
 	
 	lda	$d021
 	clc
